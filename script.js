f15ed8ac4d669125fac5eaf94f893bb5044bb87f
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-02-28 16:25:26 +0400

    Added new quote source
    Mixed response HTTPS error makes Storm Consultancy quote API useless.
    Found new quote source 'quoteondesign' and needs some work.

--- script.js
+++ script.js
@@ -6,7 +6,54 @@ $(document).ready(function(){
   //First call to get the quote while initial loading
   getQuote();
 
-	//getQuote function definition http://quotes.stormconsultancy.co.uk/random.json
+	/* getQuote function definition http://quotes.stormconsultancy.co.uk/random.json
+    
+    function getQuote(){
+		$.getJSON("http://quotes.stormconsultancy.co.uk/random.json",
+						 function(json){
+
+								quote = json.quote;
+								author = json.author;
+
+								console.log(quote.length);
+								loadData();
+		})
+
+	} */ // OLD FUNCTON
+
+    
+     function getQuote(){
+
+        $.ajax( {
+          url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+          success: function(data) {
+            var oneQuote = data.shift(); // The data is an array of posts. Grab the first one.
+              
+            console.log(oneQuote.length, oneQuote.title, oneQuote.content);  
+              
+            $('#quote-title').text(oneQuote.title);
+            $('#quote-content').html(oneQuote.content);
+
+            // If the Source is available, use it. Otherwise hide it.
+            if (typeof oneQuote.custom_meta !== 'undefined' && typeof oneQuote.custom_meta.Source !== 'undefined') {
+              $('#quote-source').html('Source:' + oneQuote.custom_meta.Source);
+            } else {
+              $('#quote-source').text('');
+            }
+          },
+          cache: false
+        });
+
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	function getQuote(){
 		$.getJSON("http://quotes.stormconsultancy.co.uk/random.json",
 						 function(json){
